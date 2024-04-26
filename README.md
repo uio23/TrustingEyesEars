efc00bd0eeb60282b81a27489b0849cd1931d6fb
uio23-laptop <98493983+uio23@users.noreply.github.com>
2024-04-26 18:35:02 +1200

    comment the userscript and refactor some of it

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TrustingEyesEars
+
+NOTE: In the userscript, when imgData is said to be an Array, it is actually a 'Uint8ClampedArray'. This might be worth naming properly in the documentation at some point.
--- userscript.js
+++ userscript.js
@@ -9,138 +9,207 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';
-    const singleImg = document.querySelector("img");
+	'use strict';
+	const singleImg = document.querySelector("img");
 
-    function fetchStringFromRGBA(rgba) {
-        let rgbaString = String(rgba[0]) + String(rgba[1]) + String(rgba[2]) + String(rgba[3]);
+	var canvas = document.createElement("canvas");
+	var context = canvas.getContext('2d');
 
-        return rgbaString;
-    }
+	/**
+		* Calculates the SHA256 digest of a given String and returns its
+		* hex String value.
+		*
+		* @param {String} input The String to hash.
+		* @return Hex String of the input's SHA256 digest.
+		*/
+	async function getSHA256Hash(input) {
+		// Encode input as utf8
+		const utf8 = new TextEncoder().encode(input);
 
-    // This could be done with async/await for better efficiency, maybe
-    function getSHA256Hash(input) {
-        // https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest#converting_a_digest_to_a_hex_string
-        const utf8 = new TextEncoder().encode(input);
-        return crypto.subtle.digest("SHA-256", utf8).then((hashBuffer) => {
-           // convert resutling ArrayBuffer to Array
-            const hashArray = Array.from(new Uint8Array(hashBuffer));
-            // convert resulting Array to string
-            const hash = hashArray.map((item) => item.toString(16).padStart(2, "0")).join("");
-            return hash;
-        });
-    };
+		// Return digest converted to hex String
+		return await crypto.subtle.digest("SHA-256", utf8).then((digestBuffer) => {
+			// https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest#converting_a_digest_to_a_hex_string
+			// convert resutling ArrayBuffer digest to Array
+			const hashArray = Array.from(new Uint8Array(digestBuffer));
+			// convert resulting Array to String
+			const hash = hashArray.map((item) => item.toString(16).padStart(2, "0")).join("");
 
-    // https://stackoverflow.com/questions/30453726/how-do-i-access-change-pixels-in-a-javascript-image-object
-    function getColorValue(imgWidth, rgbaArray, color, x, y) {
-        switch (color) {
-            case 'r':
-                return rgbaArray[((imgWidth * y) + x) * 4];
-            case 'g':
-                return rgbaArray[((imgWidth * y) + x) * 4 + 1];
-            case 'b':
-                return rgbaArray[((imgWidth * y) + x) * 4 + 2];
-            case 'a':
-                return rgbaArray[((imgWidth * y) + x) * 4 + 3];
-            default:
-                throw new Error();
-        }
-    }
+			return hash;
+		});
+	};
 
 
-    function generateAvg(centerX, centerY, imgData, imgWidth, imgHeight) {
-        var targetX = centerX + 24;
-        var targetY = centerY + 24
-
-        var redTotal = 0;
-        var greenTotal = 0;
-        var blueTotal = 0;
-        var aTotal = 0;
-
-        var redAvg, greenAvg, blueAvg, aAvg;
-        var avgPixel;
-
-        var validCount = 0;
-
-        for (let x = centerX - 24; x < targetX; x++) {
-            for (let y = centerY - 24; y < targetY; y++) {
-                if (x <= 0 || y <= 0 || x > imgWidth || y > imgHeight) {
-                    continue;
-                }
-                validCount++;
-
-                redTotal += getColorValue(imgWidth, imgData, 'r', x - 1, y - 1);
-                greenTotal += getColorValue(imgWidth, imgData, 'g', x - 1, y - 1);
-                blueTotal += getColorValue(imgWidth, imgData, 'b', x - 1, y - 1);
-                aTotal += getColorValue(imgWidth, imgData, 'a', x - 1, y - 1);
-            }
-        }
-
-        redAvg = Number((redTotal / validCount).toPrecision(3));
-        greenAvg = Number((greenTotal / validCount).toPrecision(3));
-        blueAvg = Number((blueTotal / validCount).toPrecision(3));
-        aAvg = Number((aTotal / validCount).toPrecision(3));
-
-        avgPixel = [redAvg, greenAvg, blueAvg, aAvg];
-        return avgPixel;
-    }
-
-    function initContext()
-{
-    var canvas = document.createElement("canvas");
-    var ctx = canvas.getContext('2d');
-    return [ctx, canvas];
-}
-
-function loadImage(imageSource, context, canvas)
-{
-    var imageObj = new Image();
-    imageObj.crossOrigin = 'anonymous';
-    imageObj.src = imageSource;
-    imageObj.onload = function()
-    {
-        canvas.width = imageObj.width;
-        canvas.height = imageObj.height;
-        context.drawImage(imageObj, 0, 0);
-        var imageData = context.getImageData(0,0,imageObj.width,imageObj.height);
-        readImage(imageData, imageObj.width, imageObj.height);
-    };
+	// https://stackoverflow.com/questions/30453726/how-do-i-access-change-pixels-in-a-javascript-image-object
+	/**
+		* Returns the color value of a pixel at the given location in an image. 
+		*
+		* @param {Array} rgbaArray Array of all the rgba values in relevant image.
+		* @param {number} imgWidth Width of the image. 
+		* @param {char} color The desired color.
+		* @param {number} x Pixel's x co-ordinate.
+		* @param {number} y Pixel's y co-ordinate.
+		* @return Value of the desired color of the pixel.
+		*/
+	function getColorValue(rgbaArray, imgWidth, color, x, y) {
+		switch (color) {
+			case 'r':
+				return rgbaArray[((imgWidth * y) + x) * 4];
+			case 'g':
+				return rgbaArray[((imgWidth * y) + x) * 4 + 1];
+			case 'b':
+				return rgbaArray[((imgWidth * y) + x) * 4 + 2];
+			case 'a':
+				return rgbaArray[((imgWidth * y) + x) * 4 + 3];
+			default:
+				throw new Error();
+		}
+	}
 
 
-    return imageObj;
-}
+	/**
+		* Calculates the color average of the, at-most 2401 (49 x 49), pixels
+		* in a cube around a given target pixel location.
+		*
+		* @param {Array} rgbaArray Array of all the rgba values in relevant image.
+		* @param {number} imgWidth Width of the image.
+		* @param {number} imgHeight Height of the image.
+		* @param {number} centerX x co-ordinate of target pixel.
+		* @param {number} centerY y co-ordinate of target pixel.
+		* @return An Array containing the average rgba value around target pixel.
+		*/
+	function calculateAvg(rgbaArray, imgWidth, imgHeight, centerX, centerY) {
+		var redAvg, greenAvg, blueAvg, alphaAvg;
+		var avgPixel;
 
-function readImage(imageData, w, h)
-{
-    var topLeft = generateAvg(1, 1, imageData.data, w, h);
-    var topRight = generateAvg(w, 1, imageData.data, w, h);
-    var bottomLeft = generateAvg(1, h, imageData.data, w, h);
-    var bottomRight = generateAvg(w, h, imageData.data, w, h);
+		var redTotal = 0;
+		var greenTotal = 0;
+		var blueTotal = 0;
+		var alphaTotal = 0;
 
-    console.log(topLeft);
-    console.log(topRight);
-    console.log(bottomLeft);
-    console.log(bottomRight);
+		// Track how many pixel actually exist in the average-square
+		var validCount = 0;
 
-    var rgbaStringTL = fetchStringFromRGBA(topLeft);
-    var rgbaStringTR = fetchStringFromRGBA(topRight);
-    var rgbaStringBL = fetchStringFromRGBA(bottomLeft);
-    var rgbaStringBR = fetchStringFromRGBA(bottomRight);
+		// Calc the top left 
+		var startingX = centerX - 24;
+		var startingY = centerY - 24
+		// And bottom right
+		var targetX = centerX + 24;
+		var targetY = centerY + 24;
+		// ...corners of average square
 
-    console.log(rgbaStringTL);
-    console.log(rgbaStringTR);
-    console.log(rgbaStringBL);
-    console.log(rgbaStringBR);
+		// Go across and down pixels in average square
+		for (let x = startingX; x < targetX; x++) {
+			for (let y = startingY; y < targetY; y++) {
+				// Skip if this pixel doesn't exit
+				if (x <= 0 || y <= 0 || x > imgWidth || y > imgHeight) {
+					continue;
+				}
+				// Account for this pixels existence
+				validCount++;
 
-    getSHA256Hash(rgbaStringTL).then((result) => console.log(result));
-    getSHA256Hash(rgbaStringTR).then((result) => console.log(result));
-    getSHA256Hash(rgbaStringBL).then((result) => console.log(result));
-    getSHA256Hash(rgbaStringBR).then((result) => console.log(result));
-}
+				// increment the rgba totals by the color values of this pixel
+				redTotal += getColorValue(rgbaArray, imgWidth, 'r', x - 1, y - 1);
+				greenTotal += getColorValue(rgbaArray, imgWidth, 'g', x - 1, y - 1);
+				blueTotal += getColorValue(rgbaArray, imgWidth, 'b', x - 1, y - 1);
+				alphaTotal += getColorValue(rgbaArray, imgWidth, 'a', x - 1, y - 1);
+			}
+		}
 
-var c = initContext();
-var imageObj = loadImage(singleImg.src, c[0], c[1]);
+		// Calc rgba averages to nearest whole
+		redAvg = Math.round(Number((redTotal / validCount)));
+		greenAvg = Math.round(Number((greenTotal / validCount)));
+		blueAvg = Math.round(Number((blueTotal / validCount)));
+		alphaAvg = Math.round(Number((alphaTotal / validCount)));
+
+		// Assemble the average pixel Array
+		avgPixel = [redAvg, greenAvg, blueAvg, alphaAvg];
+
+		return avgPixel;
+	}
 
 
+	/**
+		* Clones an image from the given source and calls subsequent examination
+		* functions on it, once it loads.
+		*
+		* @param {String} imgSrc URL of the relevant image.
+		* @param {HTMLCanvasElement} canvas A canvas that the image can be drawn on.
+		* @param {CanvasRenderingContext2D} context The drawing surface for the image. 
+		*/
+	function loadImage(imgSrc, canvas, context) {
+		var imgData;
 
+		var img = new Image();
+
+		// Avoid CORS violations
+		img.crossOrigin = 'anonymous';
+		
+		img.src = imgSrc;
+		img.onload = function() {
+				// Adjust the canvas to fit the image
+				canvas.width = img.width;
+				canvas.height = img.height;
+
+				// Draw the image onto the canvas's context
+				context.drawImage(img, 0, 0);
+
+				// extract the data of the image copy 
+				imgData = context.getImageData(0, 0, img.width, img.height);
+
+				// Calculate the averages and then verify them
+				calculateAvgs(imgData.data, img.width, img.height).then((avgs) => {
+					console.log("Verifying the following hashes, calculated from the averages:");
+					console.log(avgs);
+					//verifyAvgs(avgs);
+				});
+		}
+	}
+
+
+	/**
+		* Calculates the average rgba values for the four corner pixels of the image
+		* described by rgbaArray
+		*
+		* @param {Array} rgbaArray Array of all the rgba values in relevant image.
+		* @param {number} imgWidth Width of the image.
+		* @param {number} imgHeight Height of the image.
+		* @return An Array containing the average rgba values around the image's
+		*         corner pixels.
+		*/
+	async function calculateAvgs(rgbaArray, imgWidth, imgHeight) {
+		var topLeft = calculateAvg(rgbaArray, imgWidth, imgHeight, 1, 1);
+		var topRight = calculateAvg(rgbaArray, imgWidth, imgHeight, imgWidth, 1);
+		var bottomLeft = calculateAvg(rgbaArray, imgWidth, imgHeight, 1, imgHeight);
+		var bottomRight = calculateAvg(rgbaArray, imgWidth, imgHeight, imgWidth, imgHeight);
+
+		console.log("The corner pixels average rgba values follow:");
+		console.log(topLeft);
+		console.log(topRight);
+		console.log(bottomLeft);
+		console.log(bottomRight);
+		console.log("-----");
+
+		topLeft = topLeft.toString();
+		topRight = topRight.toString();
+		bottomLeft = bottomLeft.toString();
+		bottomRight = bottomRight.toString();
+
+		console.log("Their string representations follow:");
+		console.log("Top left: " + topLeft);
+		console.log("Top right: " + topRight);
+		console.log("Bottom left: " + bottomLeft);
+		console.log("Bottom right: " + bottomRight);
+		console.log("-----");
+
+		topLeft = await getSHA256Hash(topLeft);
+		topRight = await getSHA256Hash(topRight);
+		bottomLeft = await getSHA256Hash(bottomLeft);
+		bottomRight = await getSHA256Hash(bottomRight);
+
+		return [topLeft, topRight, bottomLeft, bottomRight];
+	}
+
+	// Proccess the singular first image of on the webpage
+	// Will get extended to loop through all images
+	loadImage(singleImg.src, canvas, context);
 })();
