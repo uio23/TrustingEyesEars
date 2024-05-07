51e2162f06459b4d44a6fd7372f578de0d1d6e27
uio23-laptop <98493983+uio23@users.noreply.github.com>
2024-05-07 13:21:02 +1200

    remove breaking code

--- userscript.js
+++ userscript.js
@@ -201,25 +201,6 @@
 	loadImage(singleimg.src, canvas, context).then(subjectImg => {
 		calculateID(subjectImg).then(id => {
 			console.log(id);
-			fetch('localhost:3000')
-    .then(
-    function(response) {
-        if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' +
-            response.status);
-        return;
-        }
-
-        // Examine the text in the response
-        response.json().then(function(data) {
-        console.log(data);
-        });
-    }
-    )
-    .catch(function(err) {
-    console.log('Fetch Error :-S', err);
-    });
-
 		});
 	});
 })();
