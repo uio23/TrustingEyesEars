7d74856a23ff0e605ef2b4b5f6ca8b0e89e1bb5c
uio23-laptop <98493983+uio23@users.noreply.github.com>
2024-05-09 11:25:06 +1200

    start styling view

--- server/index.js
+++ server/index.js
@@ -31,6 +31,7 @@ app.use(morgan("combined"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "views", "index.html"));
---
+++ server/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/*.html"],
+  theme: {
+    colors: {
+        'blue': '#6DD3CE',
+        'lgreen': '#C8E9A0',
+        'llgreen': '#D9EFBD',
+        'dgreen': '#397367',
+        'red': '#A13D63',
+        'purple': '#351E29',
+        
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
